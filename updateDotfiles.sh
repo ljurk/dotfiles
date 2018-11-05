7c7bc2bb33b21677c44e9d7bdab0cf883e6b9d8f
ljurk <ljurk@protonmail.com>
2018-11-05 09:25:38 +0100

    added tmux resurect, .gitignore

--- updateDotfiles.sh
+++ updateDotfiles.sh
@@ -3,13 +3,17 @@ for D in `find . -name ".*" -not -path "./.git" `
 do
     echo $D
 done
-
+#move old files
 mv ~/.tmux.conf ~/.tmux.conf.old
 mv ~/.vimrc ~/.vimrc.old
 mv ~/.bashrc ~/.bashrc.old
+mv ~/.vim ~/.vim.old
+mv ~/.config ~/.config.old
 
+#link to new files
 printf "so $HOME/dotfiles/vimrc" > ~/.vimrc
 printf "source-file $HOME/dotfiles/tmux.conf" > ~/.tmux.conf
 printf "source $HOME/dotfiles/bashrc" > ~/.bashrc
-ln -s dotfiles/.vim/ .vim
 
+ln -s $HOME/dotfiles/.vim/ ~/.vim
+ln -s $HOME/dotfiles/.config ~/.config
