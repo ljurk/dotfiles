f7e61f62c7e535b372b1dff47b67f64fdd1c092a
ljurk <ljurk@protonmail.com>
2018-11-12 18:46:02 +0000

    new alias for nextcloud, scipt now also works as sudo

--- updateDotfiles.sh
+++ updateDotfiles.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-for D in `find . -name ".*" -not -path "./.git" `
-do
-    echo $D
-done
 #move old files
 mv ~/.tmux.conf ~/.tmux.conf.old
 mv ~/.vimrc ~/.vimrc.old
@@ -11,9 +7,9 @@ mv ~/.vim ~/.vim.old
 mv ~/.config ~/.config.old
 
 #link to new files
-printf "so $HOME/dotfiles/vimrc" > ~/.vimrc
-printf "source-file $HOME/dotfiles/tmux.conf" > ~/.tmux.conf
-printf "source $HOME/dotfiles/bashrc" > ~/.bashrc
+printf "so /home/ljurk/dotfiles/vimrc" > ~/.vimrc
+printf "source-file /home/ljurk/dotfiles/tmux.conf" > ~/.tmux.conf
+printf "source /home/ljurk/dotfiles/bashrc" > ~/.bashrc
 
-ln -s $HOME/dotfiles/.vim/ ~/.vim
-ln -s $HOME/dotfiles/.config ~/.config
+ln -s /home/ljurk/dotfiles/.vim/ ~/.vim
+ln -s /home/ljurk/dotfiles/.config ~/.config
