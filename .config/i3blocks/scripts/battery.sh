bfa786eacc5a20fc970565cb7e71a9bcefdcc611
ljurk <ljurk@pm.me>
2018-12-08 14:37:04 +0100

    optimize scripts

--- .config/i3blocks/scripts/battery.sh
+++ .config/i3blocks/scripts/battery.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
+BAT0=$(acpi -b |grep 'Battery 0')
+BAT=$(echo "$BAT0" | grep -E -o '[0-9][0-9]?%')
+BATSTAT=$(echo "$BAT0" | grep -c 'Charging')
 
-BAT=$(acpi -b | grep -E -o '[0-9][0-9]?%')
+COLOR="#00F00"
+CHARGE="#00FF00"
+DISCHARGE="#FF8000"
+DISCHARGELOW="#FF0000"
 
 # Full and short texts
-echo " $BAT"
-#echo "BAT: $BAT"
+if [ "$BATSTAT" -eq 1 ]
+then
+   COLOR="$CHARGE"
+else
+    if [ "${BAT%?}" -le 30 ]
+    then
+        COLOR="$DISCHARGELOW"
+    else
+        COLOR="$DISCHARGE"
+    fi
+fi
 
-# Set urgent flag below 5% or use orange below 20%
+OUT='<span foreground="'
+OUT+="$COLOR"
+OUT+='">'
+OUT+=" $BAT"
+OUT+="</span>"
+echo $OUT
+
+# Set urgent flag below 5%
 #[ ${BAT%?} -le 5 ] && exit 33
-#[ ${BAT%?} -le 20 ] && echo "#FF8000"
 
 exit 0
