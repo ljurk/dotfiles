67fa040109d3cbae29827210f9b39d5274bb300d
ljurk <ljurk@pm.me>
2018-12-20 20:04:29 +0100

    update battery script

--- .config/i3blocks/scripts/battery.sh
+++ .config/i3blocks/scripts/battery.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 BAT0=$(acpi -b |grep 'Battery 0')
 BAT=$(echo "$BAT0" | grep -E -o '[0-9][0-9]?%')
-BATSTAT=$(echo "$BAT0" | grep -c 'Charging')
+BATSTAT=$(echo "$BAT0" | grep -c -E 'Charging|Full')
+# No Battery, so its wallpower
+NOBAT=$(echo "$BAT0" | grep -c  'rate information unavailable')
 
 COLOR="#00F00"
 CHARGE="#00FF00"
 DISCHARGE="#FF8000"
 DISCHARGELOW="#FF0000"
 
+if [ "${BAT%?}" -eq 00 ]
+then
+    BAT="100%"
+fi
 # Full and short texts
 if [ "$BATSTAT" -eq 1 ]
 then
@@ -19,6 +25,11 @@ else
     else
         COLOR="$DISCHARGE"
     fi
+fi
+
+if [ "$NOBAT" -eq 1 ]
+then
+    BAT="∞"
 fi
 
 OUT='<span foreground="'
