9c1402d42aeff15323fb61b7d5e8730867cf3d39
ljurk <ljurk@pm.me>
2019-08-28 09:17:21 +0200

    extend battery script for multiple batteries

--- i3blocks/.config/i3blocks/scripts/battery.sh
+++ i3blocks/.config/i3blocks/scripts/battery.sh
@@ -1,61 +1,72 @@
 #!/bin/bash
-BAT0=$(acpi -b |grep 'Battery 0')
-BAT=$(echo "$BAT0" | grep -E -o '[0-9][0-9]?%')
-BATSTAT=$(echo "$BAT0" | grep -c -E 'Charging|Full')
-# No Battery, so its wallpower
-NOBAT=$(echo "$BAT0" | grep -c  'rate information unavailable')
-
+BAT[0]=$(acpi -b| grep 'Battery 0')
+BAT[1]=$(acpi -b| grep 'Battery 1')
 COLOR="#00F00" #green
 CHARGE="#00FF00" #green
 DISCHARGE="#FF8000" #orange
 DISCHARGELOW="#FF0000" #red
 
-if [ "$NOBAT" -eq 1 ]
-then
-    BAT="∞"
-    BATTEXT=""
-fi
+complete=''
+for i in "${BAT[@]}"
+do
+    BAT=$(echo "$i" | grep -E -o '[0-9][0-9]?%')
+    BATSTAT=$(echo "$i" | grep -c -E 'Charging|Full')
+    # No Battery, so its wallpower
+    NOBAT=$(echo "$i" | grep -c  'rate information unavailable')
 
-if [ "${BAT%?}" -eq 00 ]
-then
-    BAT="100%"
-    BATTEXT=""
-fi
 
-#green ~ charging; orange ~ discharging
-if [ "$BATSTAT" -eq 1 ]
-then
-    COLOR="$CHARGE"
-else
-    COLOR="$DISCHARGE"
-fi
+    if [ "$NOBAT" -eq 1 ]
+    then
+        BAT="∞"
+        BATTEXT=""
+    fi
 
-# show diffrent icon dependent on the battery charge
-if [ "${BAT%?}" -le 25 ]
-then
-    BATTEXT=""
-    COLOR="$DISCHARGELOW"
-elif [ "${BAT%?}" -le 25 ]
-then
-    BATTEXT=""
-elif [ "${BAT%?}" -le 50 ]
-then
-    BATTEXT=""
-elif [ "${BAT%?}" -le 75 ]
-then
-    BATTEXT=""
-elif [ "${BAT%?}" -le 90 ]
-then
-    BATTEXT=""
-fi
+    if [ "${BAT%?}" -eq 00 ]
+    then
+        BAT="100%"
+        BATTEXT=""
+    fi
 
-#print it
-OUT='<span foreground="'
-OUT+="$COLOR"
-OUT+='">'
-OUT+=" $BATTEXT$BAT"
-OUT+="</span>"
-echo $OUT
+    #green ~ charging; orange ~ discharging
+    if [ "$BATSTAT" -eq 1 ]
+    then
+        COLOR="$CHARGE"
+    else
+        COLOR="$DISCHARGE"
+    fi
+
+    # show diffrent icon dependent on the battery charge
+    if [ "${BAT%?}" -le 25 ]
+    then
+        BATTEXT=""
+        COLOR="$DISCHARGELOW"
+    elif [ "${BAT%?}" -le 25 ]
+    then
+        BATTEXT=""
+    elif [ "${BAT%?}" -le 50 ]
+    then
+        BATTEXT=""
+    elif [ "${BAT%?}" -le 75 ]
+    then
+        BATTEXT=""
+    elif [ "${BAT%?}" -le 90 ]
+    then
+        BATTEXT=""
+    fi
+
+    #print it
+    #OUT+=" $BATTEXT$BAT"
+    OUT='<span foreground="'
+    OUT+="$COLOR"
+    OUT+='">'
+    OUT+="$BATTEXT$BAT"
+    OUT+="</span>"
+    complete+="$OUT"
+done
+
+    echo $complete
+
+
 
 #sends a notification if i click on the block
 rightclick="notify-send $BATTEXT$BAT"
